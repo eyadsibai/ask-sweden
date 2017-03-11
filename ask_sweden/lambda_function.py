04a066ac965d65018ea2ff0898afe1a25a472807
Thomas Darr <me@thomasd.se>
2017-03-11 17:13:05 +0100

    End all the sessions

--- ask_sweden/lambda_function.py
+++ ask_sweden/lambda_function.py
@@ -10,17 +10,17 @@ def lambda_handler(request_obj, context=None):
 @alexa.default
 def default_handler(request):
     logger.info('default_handler')
-    return alexa.respond('There were 42 accidents in 2016.')
+    return alexa.respond("Sorry, I don't understand.", end_session=True)
 
 @alexa.request("LaunchRequest")
 def launch_request_handler(request):
     logger.info('launch_request_handler')
-    return alexa.respond('You can ask me about car accidents.')
+    return alexa.respond('You can ask me about car accidents.', end_session=True)
 
 @alexa.request("SessionEndedRequest")
 def session_ended_request_handler(request):
     logger.info('session_ended_request_handler')
-    return alexa.respond('Goodbye.')
+    return alexa.respond('Goodbye.', end_session=True)
 
 @alexa.intent('AMAZON.CancelIntent')
 def cancel_intent_handler(request):
@@ -30,9 +30,16 @@ def cancel_intent_handler(request):
 @alexa.intent('AMAZON.HelpIntent')
 def help_intent_handler(request):
     logger.info('help_intent_handler')
-    return alexa.respond('You can ask me about car accidents.')
+    return alexa.respond('You can ask me about car accidents.', end_session=True)
 
 @alexa.intent('AMAZON.StopIntent')
 def stop_intent_handler(request):
     logger.info('stop_intent_handler')
     return alexa.respond('Okay.', end_session=True)
+
+@alexa.intent('CarAccidents')
+def car_accidents_intent_handler(request):
+    logger.info('car_accidents_intent_handler')
+    city = request.get_slot_value('city')
+    year = request.get_slot_value('year')
+    return alexa.respond('There were %s accidents in %s in %s' % (year, city, year), end_session=True)
