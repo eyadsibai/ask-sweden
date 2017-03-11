d2cbeb4984de418d145ede0b2a9c67cea4837452
Eyad Sibai <eyad.alsibai@gmail.com>
2017-03-11 18:17:37 +0100

    Add logger + cast year to int

--- ask_sweden/lambda_function.py
+++ ask_sweden/lambda_function.py
@@ -1,42 +1,51 @@
 import logging
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 from ask import alexa
 import car_accidents
 
+
 def lambda_handler(request_obj, context=None):
     return alexa.route_request(request_obj)
+
 
 @alexa.default
 def default_handler(request):
     logger.info('default_handler')
     return alexa.respond("Sorry, I don't understand.", end_session=True)
 
+
 @alexa.request("LaunchRequest")
 def launch_request_handler(request):
     logger.info('launch_request_handler')
     return alexa.respond('You can ask me about car accidents.', end_session=True)
+
 
 @alexa.request("SessionEndedRequest")
 def session_ended_request_handler(request):
     logger.info('session_ended_request_handler')
     return alexa.respond('Goodbye.', end_session=True)
 
+
 @alexa.intent('AMAZON.CancelIntent')
 def cancel_intent_handler(request):
     logger.info('cancel_intent_handler')
     return alexa.respond('Okay.', end_session=True)
+
 
 @alexa.intent('AMAZON.HelpIntent')
 def help_intent_handler(request):
     logger.info('help_intent_handler')
     return alexa.respond('You can ask me about car accidents.', end_session=True)
 
+
 @alexa.intent('AMAZON.StopIntent')
 def stop_intent_handler(request):
     logger.info('stop_intent_handler')
     return alexa.respond('Okay.', end_session=True)
+
 
 @alexa.intent('CarAccidents')
 def car_accidents_intent_handler(request):
@@ -44,7 +53,7 @@ def car_accidents_intent_handler(request):
     logger.info(request.get_slot_map())
     city = request.get_slot_value('city')
     year = request.get_slot_value('year')
-    num_card_acc = car_accidents.get_num_accidents(year=year, city=city)
+    num_card_acc = car_accidents.get_num_accidents(year=int(year), city=city)
     logger.info('%s accidents in %s in %s', num_card_acc, city, year)
     return alexa.respond(
         '''
@@ -56,6 +65,7 @@ def car_accidents_intent_handler(request):
           </speak>
         ''' % (num_card_acc, city, year),
         end_session=True, is_ssml=True)
+
 
 @alexa.intent('PopulationSweden')
 def population_intent_handler(request):
