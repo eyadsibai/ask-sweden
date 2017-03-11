6d37b978b9cd7d418423290c8d98b8fe0d2b623a
Eyad Sibai <eyad.alsibai@gmail.com>
2017-03-11 17:35:55 +0100

    add real car accidents data

---
+++ ask_sweden/car_accidents.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def read_data():
+    return pd.read_csv('data/car_accidents.csv.gz', encoding='UTF-8')
+
+
+def get_num_accidents(year, city):
+    df = read_data()
+    year_data = df['ar'] == year
+    city_data = df['kommun'] == city
+    return len(df[year_data & city_data])
